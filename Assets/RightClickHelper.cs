7a2066099f1ab8a8004b7aed47030800adc4cecb
BeanMachineStudios <Benjaminkeller@gmail.com>
2019-07-23 18:14:17 -0700

    uggg!

---
+++ Assets/RightClickHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightClickHelper : MonoBehaviour
+{
+    public GameObject Mass;
+    public GameObject Charge;
+    public GameObject Elastic;
+    public GameObject Color1;
+    public GameObject Color2;
+    public GameObject Color3;
+    public GameObject Color4;
+    public GameObject Size1;
+    public GameObject Size2;
+}
--- Assets/drag.cs
+++ Assets/drag.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class drag : MonoBehaviour
 {
@@ -61,11 +62,20 @@ public class drag : MonoBehaviour
             {
                 rightMenu.transform.position = Input.mousePosition + menuOffest;
                 rightMenu.SetActive(true);
+                UpdateRightMenuStats();
             }
         }
         if (Input.GetMouseButtonDown(0))
         {
             rightMenu.SetActive(false);
         }
+    }
+
+    private float num;
+    private void UpdateRightMenuStats()
+    {
+        num = float.Parse(GameObject.Find("RightMenuInputField 1").GetComponent<InputField>().text); 
+        num = gameObject.GetComponent<Rigidbody>().mass;
+        Debug.Log("adjifg");
     }
 }
