74656361655f1707488d9be89cd81050c6dd1574
BeanMachineStudios <Benjaminkeller@gmail.com>
2019-07-28 21:33:08 -0700

    fixed zoom bug

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -10,15 +10,19 @@ public class CameraController : MonoBehaviour
     public GameObject cubeDown;
     public GameObject cubeLeft;
     public GameObject cubeRight;
+    private int zoomCounter;
+    private bool zoomLocked;
     //private int min = 1;
     //public float size;
 
-    // void update()
-    // {
-    //size = Camera.main.GetComponent<Camera>().orthographicSize;
-    // }
+     void update()
+     {
+     //size = Camera.main.GetComponent<Camera>().orthographicSize;
+     }
     void Start()
     {
+        zoomCounter = 1;
+        zoomLocked = false;
         cubeDown.transform.position = Camera.main.ViewportToWorldPoint(new Vector3((Camera.main.rect.xMax / 2f), Camera.main.rect.yMin, 10));
         cubeUp.transform.position = Camera.main.ViewportToWorldPoint(new Vector3((Camera.main.rect.xMax / 2f), Camera.main.rect.yMax, 10));
         cubeLeft.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(Camera.main.rect.xMin, (Camera.main.rect.yMax / 2f), 10));
@@ -28,13 +32,21 @@ public class CameraController : MonoBehaviour
     public void ZoomOut()
     {
        if (Camera.main.GetComponent<Camera>().orthographicSize < NumberOfZoomLevels * 8)
-        {
+       {
             Camera.main.GetComponent<Camera>().orthographicSize += 8;
-            cubeUp.transform.position = Camera.main.ViewportToWorldPoint(new Vector3 ((Camera.main.rect.xMax / 2f), Camera.main.rect.yMin, 10));
-            cubeDown.transform.position = Camera.main.ViewportToWorldPoint(new Vector3((Camera.main.rect.xMax / 2f), Camera.main.rect.yMax, 10));
-            cubeLeft.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(Camera.main.rect.xMin, (Camera.main.rect.yMax / 2f), 10));
-            cubeRight.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(Camera.main.rect.xMax, (Camera.main.rect.yMax / 2f), 10));
-        }
+            if (zoomCounter != NumberOfZoomLevels && zoomLocked != true)
+            {
+                cubeUp.transform.position = Camera.main.ViewportToWorldPoint(new Vector3((Camera.main.rect.xMax / 2f), Camera.main.rect.yMin, 10));
+                cubeDown.transform.position = Camera.main.ViewportToWorldPoint(new Vector3((Camera.main.rect.xMax / 2f), Camera.main.rect.yMax, 10));
+                cubeLeft.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(Camera.main.rect.xMin, (Camera.main.rect.yMax / 2f), 10));
+                cubeRight.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(Camera.main.rect.xMax, (Camera.main.rect.yMax / 2f), 10));
+                zoomCounter++;
+                if (zoomCounter == NumberOfZoomLevels)
+                {
+                    zoomLocked = true;
+                }
+            }
+       }
     }
 
     public void ZoomIn()
@@ -42,6 +54,7 @@ public class CameraController : MonoBehaviour
         if (Camera.main.GetComponent<Camera>().orthographicSize > 8 /*min*8*/)
         {
             Camera.main.GetComponent<Camera>().orthographicSize -= 8;
+            zoomCounter--;
         }
     }
 }
