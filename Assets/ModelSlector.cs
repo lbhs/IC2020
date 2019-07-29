a10d832e14eb59547f8e041d6e4e47318966bf5f
BeanMachineStudios <Benjaminkeller@gmail.com>
2019-07-28 18:37:02 -0700

    started work on the model slector

--- Assets/ModelSlector.cs
+++ Assets/ModelSlector.cs
@@ -2,25 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class main : MonoBehaviour
+public class ModelSlector : MonoBehaviour
 {
-    public float G;
-    public float k;
-    //public int n;
-	
+    public GameObject Pannel;
+    public int n;
+    // Start is called before the first frame update
     void Start()
     {
-        //ui stuff here
-		
-		//initializes forces
-        gameObject.AddComponent<forces>().initialize(G, k);
+        
+    }
 
-        /*
-		//example randomly adds several of 2 different kinds of particles
-        for(int x = 0; x < n; x++)
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenEmptyScene()
+    {
+        Pannel.SetActive(false);
+    }
+}
+        //example randomly adds several of 2 different kinds of particles
+        /*for (int x = 0; x < n; x++)
         {
             gameObject.GetComponent<forces>().addSphere(1.0f, 1, true, new Vector3(UnityEngine.Random.Range(-5, 5), UnityEngine.Random.Range(-5, 5), 0), Color.blue, 1);
             gameObject.GetComponent<forces>().addSphere(2.0f, 2, true, new Vector3(UnityEngine.Random.Range(-5, 5), UnityEngine.Random.Range(-5, 5), 0), Color.red, 2);
         }*/
-    }
-}
