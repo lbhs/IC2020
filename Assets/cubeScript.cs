685fd47d6f085c2a68a823f8c8353c1dd579d56b
BeanMachineStudios <Benjaminkeller@gmail.com>
2019-07-30 10:39:01 -0700

    cube test 1

---
+++ Assets/cubeScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeScript : MonoBehaviour
+{
+    public Vector3 Velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Rigidbody>().velocity = Velocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.GetComponent<Rigidbody>().velocity.sqrMagnitude < 50)
+        {
+            gameObject.GetComponent<Rigidbody>().velocity = gameObject.GetComponent<Rigidbody>().velocity*1.4f;
+        }
+    }
+}
