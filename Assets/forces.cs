92e96e1d0d518d10372412cfaac494f26f5518a9
BeanMachineStudios <Benjaminkeller@gmail.com>
2019-08-02 11:09:30 -0700

    changes to buffet table

--- Assets/forces.cs
+++ Assets/forces.cs
@@ -101,9 +101,9 @@ public class forces : MonoBehaviour
         float hhx = 0f;
         float hhy = .75f;
 
-        GameObject hydrogen = gameObject.GetComponent<forces>().addSphere(1.0f, .1f, new Vector3(xd + hydrox, yd + hydroy, 0), Color.yellow, 1, 0.2f , 2);
-        GameObject oxygen = gameObject.GetComponent<forces>().addSphere(2.0f, -.2f, new Vector3(xd, yd, 0), Color.green, 2, 0.2f, 2);
-        GameObject hh = gameObject.GetComponent<forces>().addSphere(1.0f, .1f, new Vector3(xd + hhx, yd + hhy, 0), Color.yellow, 1, 0.2f, 2);
+        GameObject hydrogen = gameObject.GetComponent<forces>().addSphere(1.0f, .1f, new Vector3(xd + hydrox, yd + hydroy, 0), Color.white, 1, 0.2f , 2);
+        GameObject oxygen = gameObject.GetComponent<forces>().addSphere(2.0f, -.2f, new Vector3(xd, yd, 0), Color.red, 2, 0.2f, 2);
+        GameObject hh = gameObject.GetComponent<forces>().addSphere(1.0f, .1f, new Vector3(xd + hhx, yd + hhy, 0), Color.white, 1, 0.2f, 2);
 
         //adding tags
         hydrogen.tag = "isInWater";
