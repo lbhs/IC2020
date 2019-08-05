224237310929117939468879c4e199a437053537
Kennelbound <kennelbound@users.noreply.github.com>
2019-08-05 15:05:52 -0700

    Adding additional logging messages to the build

--- ci/post_deploy.sh
+++ ci/post_deploy.sh
@@ -13,10 +13,18 @@ git stash
 git config --replace-all remote.origin.fetch +refs/head/*:refs/remotes/origin/*
 git fetch
 
+echo "Checking out gh-pages branch"
+cd ..
+git clone git@github.com:lbhs/IC2020 tmp-ic2020
+cd tmp-ic2020
+
 # Change to the gh-pages branch and update the submodules
+echo "Before checkout"
 git checkout -t -b origin/gh-pages
+echo "Before updating submodules"
 git submodule update --recursive --remote --merge --init
 git add -A .
+echo "Before commit: $(git status)"
 git commit -m "Updating submodules..." 
 git push origin gh-pages --force
 
