fa83542012a74af9fed7db769fa7f22a584329b8
Kennelbound <kennelbound@users.noreply.github.com>
2019-08-05 14:11:16 -0700

    Adding travis-ci git cleanup commands

--- ci/post_deploy.sh
+++ ci/post_deploy.sh
@@ -5,11 +5,19 @@ set -e
 echo "Starting the post-deployment update to gh-pages."
 echo "Using git version: $(git --version)"
 
+# Stash the existing changes
 git add -A .
 git stash
-git fetch origin gh-pages
+
+# Setup the repo for fetching normally
+git config --replace-all remote.origin.fetch +refs/head/*:refs/remotes/origin/*
+git fetch
+
+# Change to the gh-pages branch and update the submodules
 git checkout -t -b origin/gh-pages
 git submodule update --recursive --remote --merge --init
 git add -A .
 git commit -m "Updating submodules..." 
 git push origin gh-pages --force
+
+echo "Completed updating the gh-pages submodules."
