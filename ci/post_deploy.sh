6140423c8434e1d330151656659f46de4c828ce5
Sam Alston <github@kennelbound.com>
2019-08-04 23:25:02 -0700

    Fetching the origin before attempting to merge it

--- ci/post_deploy.sh
+++ ci/post_deploy.sh
@@ -4,8 +4,9 @@ set -e
 
 git add -A .
 git stash
+git fetch origin gh-pages
 git checkout -b gh-pages -t origin/gh-pages
-git submodule update --recursive --remote --rebase --init
+git submodule update --recursive --remote --merge --init
 git add -A .
 git commit -m "Updating submodules..." 
 git push origin gh-pages --force
