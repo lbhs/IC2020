42df6cb23ae51f66bedf6e6a1fbad99130a764f5
Benny <bennyd87708@gmail.com>
2019-08-05 09:36:35 -0700

    update

--- Assets/cubeScript.cs
+++ Assets/cubeScript.cs
@@ -1,34 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class cubeScript : MonoBehaviour
 {
 
     public Vector3 velocity;
     public int velocitySpeedUp;
+	private Rigidbody rb;
+	public int temp;
+	private Slider temperatureSlider;
     // Start is called before the first frame update
     void OnEnable()
     {
+		temperatureSlider = GameObject.Find("temperature").GetComponent<Slider>();
+		
+		rb = gameObject.GetComponent<Rigidbody>();
+		
         float vx = UnityEngine.Random.Range(-5, 6);
-        float vy = Mathf.Sqrt(50 - (vx * vx));
-        float vPlaceholder = UnityEngine.Random.Range(0, 2) *2 -1;
-        //print(vPlaceholder);
-        
+        float vy = Mathf.Sqrt(50 - (vx * vx));        
       
-        velocity = new Vector3(vx, vy *vPlaceholder, 0);
+        velocity = new Vector3(vx, vy, 0);
 
-        gameObject.GetComponent<Rigidbody>().velocity = velocity;
+        rb.velocity = velocity;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(gameObject.GetComponent<Rigidbody>().velocity.sqrMagnitude < 50)
+		temp = (int)temperatureSlider.value;
+        if(rb.velocity.sqrMagnitude < 50)
         {
             //print("old velocity =" + velocity);
-            gameObject.GetComponent<Rigidbody>().velocity = gameObject.GetComponent<Rigidbody>().velocity.normalized * 5 * Mathf.Sqrt(2);
+            rb.velocity = rb.velocity.normalized * 5 * Mathf.Sqrt(2);
             //print("new velocity =" + velocity);
         }
+		Debug.Log(temp);
     }
 }
+
