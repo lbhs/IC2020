fceafdef19913adf4bccd923bb582712309d2482
Kennelbound <kennelbound@users.noreply.github.com>
2019-08-05 12:54:15 -0700

    Adding support for integ tag and config for deploying

--- ci/post_deploy.sh
+++ ci/post_deploy.sh
@@ -2,11 +2,29 @@
 
 set -e
 
+echo "Starting the post-deployment update to gh-pages."
+
+# Stash the existing changes
 git add -A .
 git stash
-git fetch origin gh-pages
-git checkout -t -b origin/gh-pages
+
+# Setup the repo for fetching normally
+echo "Fetching origin:"
+git fetch origin
+
+# Change to the gh-pages branch and update the submodules
+echo "Checking out gh-pages branch"
+git checkout -b gh-pages -t origin/gh-pages
+
+echo "Before updating submodules"
 git submodule update --recursive --remote --merge --init
 git add -A .
-git commit -m "Updating submodules..." 
+
+echo "Before commit: $(git status)"
+git commit -m "Updating submodules..."
+
+# Decrypt the github key 
+export GIT_SSH_COMMAND="ssh -i $(pwd)/lbhs_github_cert"
 git push origin gh-pages --force
+
+echo "Completed updating the gh-pages submodules."
