5b1ac7b6a9312b78158534496edd865d0471b754
Kennelbound <kennelbound@users.noreply.github.com>
2019-08-05 16:42:13 -0700

    Adding more debugging details

--- ci/post_deploy.sh
+++ ci/post_deploy.sh
@@ -10,20 +10,18 @@ git add -A .
 git stash
 
 # Setup the repo for fetching normally
+echo "Setting up the repo"
 git config --replace-all remote.origin.fetch +refs/head/*:refs/remotes/origin/*
 git fetch
 
-echo "Checking out gh-pages branch"
-cd ..
-git clone git@github.com:lbhs/IC2020 tmp-ic2020
-cd tmp-ic2020
-
 # Change to the gh-pages branch and update the submodules
-echo "Before checkout"
-git checkout -t -b origin/gh-pages
+echo "Checking out gh-pages branch"
+git checkout -b gh-pages-sub -t origin/gh-pages
+
 echo "Before updating submodules"
 git submodule update --recursive --remote --merge --init
 git add -A .
+
 echo "Before commit: $(git status)"
 git commit -m "Updating submodules..." 
 git push origin gh-pages --force
