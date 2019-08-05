0e5611b5a54335b3cf34afcf489eed9e461acfca
Benny <bennyd87708@gmail.com>
2019-08-05 11:17:54 -0700

    fixes

--- Assets/cubeScript.cs
+++ Assets/cubeScript.cs
@@ -1,37 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class cubeScript : MonoBehaviour
 {
 
 	public Vector3 velocity;
-	public int velocitySpeedUp;
+	public float temp;
+	private Rigidbody cube;
+	private Slider temperatureSlider;
+	
 	// Start is called before the first frame update
 	void OnEnable()
 	{
+		cube = gameObject.GetComponent<Rigidbody>();
+		temperatureSlider = GameObject.Find("temperatureSlider").GetComponent<Slider>();
+		
 		float vx = UnityEngine.Random.Range(-5, 6);
 		float vy = Mathf.Sqrt(50 - (vx * vx));
-		float vPlaceholder = UnityEngine.Random.Range(0, 2) *2 -1;
-		//print(vPlaceholder);
+		velocity = new Vector3(vx, vy, 0);
+		cube.velocity = velocity;
+		temp = temperatureSlider.value;
 		
-	  
-		velocity = new Vector3(vx, vy *vPlaceholder, 0);
 		GameObject.Find("GameObject").GetComponent<forces>().nonObjects.Add(gameObject);
-		gameObject.GetComponent<Rigidbody>().velocity = velocity;
 	}
 
 	// Update is called once per frame
 	void FixedUpdate()
 	{
-		if (Time.timeScale != 0 && GameObject.Find("GameObject").GetComponent<forces>().recording)
+		temp = temperatureSlider.value;
+		
+		if (Time.timeScale != 0 && GameObject.Find("GameObject").GetComponent<forces>().recording && cube.velocity.magnitude < (7 * temp))
 		{
-			if(gameObject.GetComponent<Rigidbody>().velocity.sqrMagnitude < 50)
-			{
-				//print("old velocity =" + velocity);
-				gameObject.GetComponent<Rigidbody>().velocity = gameObject.GetComponent<Rigidbody>().velocity.normalized * 5 * Mathf.Sqrt(2);
-				//print("new velocity =" + velocity);
-			}
+			cube.velocity *= 1.4f;
+			print("new velocity = " + velocity.magnitude);
 		}
 	}
 }
