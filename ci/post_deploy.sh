aad798cfbd2b5e63e6ef9419a5a63c36ed0fea2c
Kennelbound <github@kennelbound.com>
2019-08-10 13:36:30 -0700

    Improving the release process: - builds the WebGL binaries and deploys to pages-* branch - after deployment, updates the gh-pages submodules to point to pages-*

--- ci/post_deploy.sh
+++ ci/post_deploy.sh
@@ -2,11 +2,25 @@
 
 set -e
 
-git add -A .
-git stash
-git fetch origin gh-pages
-git checkout -b gh-pages -t origin/gh-pages
+echo "Starting the post-deployment update to gh-pages: $(pwd -P)"
+
+# Setup the cert
+openssl aes-256-cbc -K ${encrypted_d4473614101c_key} -iv ${encrypted_d4473614101c_iv} -in ci/config -out config.tar -d
+tar xvf config.tar
+mv lbhs_github_cert ~/.ssh/ 
+chmod 400 ~/.ssh/lbhs_github_cert
+
+# Clone the repo
+cd ..
+GIT_SSH_COMMAND="ssh -i ~/.ssh/lbhs_github_cert" git clone git@github.com:lbhs/IC2020.git --branch gh-pages ghpages
+cd ghpages
+
+echo "Before updating submodules"
 git submodule update --recursive --remote --merge --init
 git add -A .
-git commit -m "Updating submodules..." 
-git push origin gh-pages --force
+
+echo "Before commit: $(git status)"
+git commit -m "Updating submodules..."
+GIT_SSH_COMMAND="ssh -i ~/.ssh/lbhs_github_cert" git push origin gh-pages --force
+
+echo "Completed updating the gh-pages submodules."
