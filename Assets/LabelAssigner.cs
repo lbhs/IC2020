a02f9f74a59176dd737ef68ecb7ba9eac6d2e5e3
BeanMachineStudios <Benjaminkeller@gmail.com>
2019-08-15 09:30:04 -0700

    added +/- to the prefabs

--- Assets/LabelAssigner.cs
+++ Assets/LabelAssigner.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class LabelAssigner : MonoBehaviour
 {
     // Start is called before the first frame update
-    void onEnable()
+    private int imgToUse;
+    private GameObject tempLable;
+    void OnEnable()
     {
-        int imgToUse;
         if (gameObject.GetComponent<charger>().charge < 0) imgToUse = 1;
             else imgToUse = 0;
-        GameObject tempLable;
                 tempLable = MonoBehaviour.Instantiate(GameObject.Find("Lable Canvas").GetComponent<LableManager>().imagePrefabs[imgToUse], Vector3.zero, Quaternion.identity);
                 tempLable.transform.SetParent(GameObject.Find("Lable Canvas").transform);
                 tempLable.GetComponent<ImageFollower>().sphereToFollow = gameObject;
+        Debug.Log(imgToUse);
     }
 
 }
