282bae5c82aa73cc5853610e42390d6e07596053
sgsogo <43896422+sgsogo@users.noreply.github.com>
2019-09-08 07:38:18 -0700

    Speed changed to Simulation

---
+++ Packages/Assets/Buffet Table/InvisibleBGCtrl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InvisibleBGCtrl : MonoBehaviour, IPointerClickHandler
+{
+    MenuVisibilityCtrl _parentCtrl;
+
+    public void SetParentCtrl(MenuVisibilityCtrl ctrl)
+    {
+        _parentCtrl = ctrl;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        _parentCtrl.Hide();
+    }
+}
---
+++ Packages/Assets/time.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class time : MonoBehaviour
+{
+	Text speed;
+	
+	public void updateTimeScale(float scale)
+	{
+		GameObject.Find("speedText").GetComponent<Text>().text = "Simulation: " + (scale/4).ToString() + "x";
+		if(scale < 0)
+		{
+			if(GameObject.Find("GameObject").GetComponent<TimeBody>().frame == -1)
+			{
+				Time.timeScale = 0f;
+				GetComponent<UnityEngine.UI.Slider>().value = 0;
+			}
+			else
+			{
+				GameObject.Find("GameObject").GetComponent<forces>().startRewind();
+				Time.timeScale = Math.Abs(scale/4);
+				GetComponent<UnityEngine.UI.Slider>().value = scale;
+			}
+		}
+		else
+		{
+			Time.timeScale = scale/4;
+			GetComponent<UnityEngine.UI.Slider>().value = scale;
+			GameObject.Find("GameObject").GetComponent<forces>().stopRewind();
+		}
+	}
+}
