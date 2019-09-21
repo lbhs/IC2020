bab26e66260753131572e1d87bb6c9a1f7c9a375
BeanMachineStudios <Benjaminkeller@gmail.com>
2019-09-20 21:42:13 -0700

    added method to add text onto a prefab paticle insted of just plus/minus

--- Assets/LabelAssigner.cs
+++ Assets/LabelAssigner.cs
@@ -4,17 +4,28 @@ using UnityEngine;
 
 public class LabelAssigner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool useTextLable;
+    public string Lable;
     private int imgToUse;
     private GameObject tempLable;
+
     void OnEnable()
     {
-        if (gameObject.GetComponent<charger>().charge < 0) imgToUse = 1;
+        if (!useTextLable)
+        {
+            if (gameObject.GetComponent<charger>().charge < 0) imgToUse = 1;
             else imgToUse = 0;
-                tempLable = MonoBehaviour.Instantiate(GameObject.Find("Lable Canvas").GetComponent<LableManager>().imagePrefabs[imgToUse], Vector3.zero, Quaternion.identity);
-                tempLable.transform.SetParent(GameObject.Find("Lable Canvas").transform);
-                tempLable.GetComponent<ImageFollower>().sphereToFollow = gameObject;
-        //.Log(imgToUse);
+            tempLable = MonoBehaviour.Instantiate(GameObject.Find("Lable Canvas").GetComponent<LableManager>().imagePrefabs[imgToUse], Vector3.zero, Quaternion.identity);
+            tempLable.transform.SetParent(GameObject.Find("Lable Canvas").transform);
+            tempLable.GetComponent<ImageFollower>().sphereToFollow = gameObject;
+            //.Log(imgToUse);
+        }
+        else
+        {
+            tempLable = MonoBehaviour.Instantiate(GameObject.Find("Lable Canvas").GetComponent<LableManager>().TextPrefab, Vector3.zero, Quaternion.identity);
+            tempLable.transform.SetParent(GameObject.Find("Lable Canvas").transform);
+            tempLable.GetComponent<ImageFollower>().sphereToFollow = gameObject;
+        }
     }
 
 }
