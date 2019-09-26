1533a116ffe27b24a131f928773acda53104dd10
BeanMachineStudios <Benjaminkeller@gmail.com>
2019-09-25 23:14:46 -0700

    almost finished swap anamation

--- Assets/Redox.cs
+++ Assets/Redox.cs
@@ -31,37 +31,47 @@ public class Redox : MonoBehaviour
             {
                 if (EP + otherP.EP > 0)
                 {
+                    float oldTime = Time.timeScale;
+                    Time.timeScale = 0;
                     //gets positions of both objects
                     Vector3 Rpos = gameObject.transform.position;
                     Vector3 Opos = otherP.transform.position;
 
-                    //spawn the new objects with the old coordinates but flipped
-                    GameObject P = Instantiate(otherP.ReactionPrefab, Opos, Quaternion.identity);
-                    GameObject G = Instantiate(ReactionPrefab, Rpos, Quaternion.identity);
-
-                    StartCoroutine(moveToX(G.transform, Opos, P.transform, Rpos, 1.5f, collision));
+                    StartCoroutine(moveToX(gameObject.transform, Opos, otherP.transform, Rpos, 1.5f, collision, oldTime));
                     //StartCoroutine(moveToX(P.transform, Rpos, 0.5f));
 
-                    //Destroy the old objects
-                    gameObject.name = "destroyed";
-                    mainObject.gameObjects.Remove(gameObject);
-                    Destroy(otherP.gameObject);
-
-                    otherP.gameObject.name = "destroyed";
-                    mainObject.gameObjects.Remove(otherP.gameObject);
-                    //Destroy(gameObject);
-                    transform.position = new Vector3(100, 100, 100);
-                    //The need to rename the gameobject is so that it loses the [P] tag
-                    //The tag will automatically re-add the particle to the physics list
-                    //If an object is destroyed without being removed from the physics list,
-                    //all physics will stop until it is resolved
+                   
+                    
                 }
             }
         }
     }
+
+    void finishTheJob(Redox otherP, Vector3 Opos, Vector3 Rpos)
+    {
+        //spawn the new objects with the old coordinates but flipped
+        Instantiate(otherP.ReactionPrefab, Opos, Quaternion.identity);
+        Instantiate(ReactionPrefab, Rpos, Quaternion.identity);
+
+        //Destroy the old objects
+        gameObject.name = "destroyed";
+        mainObject.gameObjects.Remove(gameObject);
+        Destroy(otherP.gameObject);
+
+        otherP.gameObject.name = "destroyed";
+        mainObject.gameObjects.Remove(otherP.gameObject);
+        //Destroy(gameObject);
+        transform.position = new Vector3(100, 100, 100);
+
+        //The need to rename the gameobject is so that it loses the [P] tag
+        //The tag will automatically re-add the particle to the physics list
+        //If an object is destroyed without being removed from the physics list,
+        //all physics will stop until it is resolved
+    }
+
     bool isMoving = false;
 
-    IEnumerator moveToX(Transform AfromPosition, Vector3 AtoPosition, Transform BfromPosition, Vector3 BtoPosition, float duration, Collision collision)
+    IEnumerator moveToX(Transform AfromPosition, Vector3 AtoPosition, Transform BfromPosition, Vector3 BtoPosition, float duration, Collision collision, float oldTime)
     {
         //Make sure there is only one instance of this function running
         if (isMoving)
@@ -76,8 +86,8 @@ public class Redox : MonoBehaviour
         Vector3 AstartPos = AfromPosition.position;
         Vector3 BstartPos = BfromPosition.position;
 
-        float oldTime = Time.timeScale;
-        Time.timeScale = 0;
+        //float oldTime = Time.timeScale;
+        //Time.timeScale = 0;
 
         zoomIn(collision.contacts[0].point);
         yield return new WaitForSecondsRealtime(0.75f);
@@ -90,11 +100,16 @@ public class Redox : MonoBehaviour
             yield return null;
         }
 
+        finishTheJob(BfromPosition.gameObject.GetComponent<Redox>(), BtoPosition, AtoPosition);
+        //To-Do add color gradient swap
+        yield return new WaitForSecondsRealtime(0.75f);
         zoomOut();
 
         Time.timeScale = oldTime;
 
         isMoving = false;
+
+       
     }
 
     private void zoomIn(Vector3 pos)
