b3f57f96ad098c2452d6e35c18d240f35c973ec9
BeanMachineStudios <Benjaminkeller@gmail.com>
2019-11-06 15:47:34 -0800

    added Asa's Energy well to Ben branch for testing via unity package
    Co-Authored-By: sirmunches <sirmunches@users.noreply.github.com>

---
+++ Assets/Scenes/EnergyWell/moveOnClick.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class moveOnClick : MonoBehaviour
+{
+    public Rigidbody bodyType;
+    Rigidbody m_Rigidbody;
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>(); //defines the rigidbody as m_Rigidbody 
+        m_Rigidbody.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY; //Freezes constraints
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("space")) //When space is pressed
+        {
+            // gameObject.GetComponent<Rigidbody>(). = !gameObject.GetComponent<Rigidbody>().useGravity;
+            m_Rigidbody.isKinematic = false; //Disables kinematics, allowing movement
+            m_Rigidbody = GetComponent<Rigidbody>();
+            m_Rigidbody.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
+        }
+        void Update()
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                // gameObject.GetComponent<Rigidbody>(). = !gameObject.GetComponent<Rigidbody>().useGravity;
+                m_Rigidbody.isKinematic = false;
+            }
+        }
+    }
+}
+
+
