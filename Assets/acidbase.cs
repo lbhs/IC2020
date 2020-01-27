1508684c952537412f44e9b8c6613af646853ea7
Benny <bennyd87708@gmail.com>
2020-01-27 09:49:54 -0800

    tangent progress

--- Assets/acidbase.cs
+++ Assets/acidbase.cs
@@ -5,68 +5,70 @@ using IC2020;
 public class acidbase : MonoBehaviour
 {
 	private forces mainObject;
-	public bool bound = false;
+	//public bool bound = false;
 	
 	//public bool collided;
     // Start is called before the first frame update
     void Start()
     {
         mainObject = GameObject.Find("GameObject").GetComponent<forces>();
+		Debug.Log("this farrrr");
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-		acidbase otherP = collision.gameObject.GetComponent<acidbase>();
-        if(otherP != null)
+		GameObject particle1 = gameObject;
+		GameObject particle2 = collision.gameObject;
+        if(particle2.GetComponent<acidbase>() != null && particle2.GetComponent<Renderer>().material.color == ICColor.Hydrogen)
 		{
-			if(collision.gameObject.GetComponent<Renderer>().material.color == ICColor.Hydrogen) //for acid base reactions only
-			{
-				Destroy(otherP); //DOESNT DESTROY PARTICLE!!!! just destroys the acidbase component
-				charger charge = GetComponent<charger>();
-						
-				//bind hydrogen to atom
-				ConfigurableJoint cjoint;
-				cjoint = collision.gameObject.AddComponent<ConfigurableJoint>();
-				cjoint.xMotion = ConfigurableJointMotion.Limited;
-				cjoint.yMotion = ConfigurableJointMotion.Limited;
-				cjoint.zMotion = ConfigurableJointMotion.Locked;
-				cjoint.angularXMotion = ConfigurableJointMotion.Limited;
-				cjoint.angularYMotion = ConfigurableJointMotion.Limited;
-				cjoint.angularZMotion = ConfigurableJointMotion.Locked;
-				cjoint.connectedBody = gameObject.GetComponent<Rigidbody>();
-				cjoint.anchor = Vector3.down;
-				cjoint.angularXMotion = ConfigurableJointMotion.Limited;
-				cjoint.angularYMotion = ConfigurableJointMotion.Limited;
-				cjoint.angularZMotion = ConfigurableJointMotion.Locked;
-				cjoint.autoConfigureConnectedAnchor = false;
-				//need to add rotations based on angle of collision with tangent probably (rotate, connect, then rotate back)
-				if(gameObject.GetComponent<Renderer>().material.color == ICColor.Oxygen && charge.charge == -1){
-					collision.gameObject.transform.position = gameObject.transform.position + new Vector3(0.5f, -0.6f, 0);
-					cjoint.connectedAnchor = new Vector3(0f, 0f, 0);
-					Destroy(GetComponent<acidbase>());
-					bound = true;
-				}
-				else
-				{
-					collision.gameObject.transform.position = gameObject.transform.position + new Vector3(0f, 0.750f, 0);
-					cjoint.connectedAnchor = new Vector3(0f, 0f, 0);
-				}
-				
-				charge.updateCharge(charge.charge + 1);
-				collision.gameObject.GetComponent<charger>().updateCharge(0);
-				
-				//finish binding hydrogen to atom
-				var limit = new SoftJointLimit();
-				limit.limit = 0.1f;
-				//limit.SoftJointLimitSpring = 40.0f;
-				cjoint.linearLimit = limit;
-
-				limit.limit = 10.0f;
-				cjoint.angularYLimit = limit;
-				cjoint.angularZLimit = limit;
-				cjoint.lowAngularXLimit = limit;
-				cjoint.highAngularXLimit = limit;
+			Destroy(collision.gameObject.GetComponent<acidbase>()); //DOESNT DESTROY PARTICLE!!!! just destroys the acidbase component - in the future it will set it as able to give away
+			charger charge = GetComponent<charger>();
+					
+			//bind hydrogen to atom
+			ConfigurableJoint cjoint;
+			cjoint = collision.gameObject.AddComponent<ConfigurableJoint>();
+			cjoint.xMotion = ConfigurableJointMotion.Limited;
+			cjoint.yMotion = ConfigurableJointMotion.Limited;
+			cjoint.zMotion = ConfigurableJointMotion.Locked;
+			cjoint.angularXMotion = ConfigurableJointMotion.Limited;
+			cjoint.angularYMotion = ConfigurableJointMotion.Limited;
+			cjoint.angularZMotion = ConfigurableJointMotion.Locked;
+			cjoint.connectedBody = gameObject.GetComponent<Rigidbody>();
+			cjoint.anchor = Vector3.down;
+			cjoint.angularXMotion = ConfigurableJointMotion.Limited;
+			cjoint.angularYMotion = ConfigurableJointMotion.Limited;
+			cjoint.angularZMotion = ConfigurableJointMotion.Locked;
+			cjoint.autoConfigureConnectedAnchor = false;
+			
+			//if second water hydrogen then connect at 104.5 degrees, otherwise 
+			if(particle1.GetComponent<Renderer>().material.color == ICColor.Oxygen && charge.charge == -1){
+				//particle2.transform.position = particle1.transform.position + new Vector3(0.5f, -0.6f, 0);
+				cjoint.connectedAnchor = new Vector3(0f, 0f, 0);
+				//Destroy(GetComponent<acidbase>()); //not in the future
+				//bound = true;
 			}
+			else
+			{
+				//collision.gameObject.transform.position = gameObject.transform.position + new Vector3(0f, 0.750f, 0);
+				//particle2.transform.position = Vector3.MoveTowards(particle2.transform.position, particle1.transform.position, particle1.transform.localScale.x/2);
+				Physics.IgnoreCollision(particle1.GetComponent<SphereCollider>(), particle2.GetComponent<SphereCollider>());
+				cjoint.connectedAnchor = new Vector3(0f, 0f, 0);
+			}
+			
+			charge.updateCharge(charge.charge + 1);
+			collision.gameObject.GetComponent<charger>().updateCharge(0);
+			
+			//finish binding hydrogen to atom
+			var limit = new SoftJointLimit();
+			limit.limit = 0.1f;
+			//limit.SoftJointLimitSpring = 40.0f;
+			cjoint.linearLimit = limit;
+
+			limit.limit = 10.0f;
+			cjoint.angularYLimit = limit;
+			cjoint.angularZLimit = limit;
+			cjoint.lowAngularXLimit = limit;
+			cjoint.highAngularXLimit = limit;
 		}
 	}
 }
