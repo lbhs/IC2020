62ca40b8b216a66ac409bef296152e849ae1f3b1
Benny <bennyd87708@gmail.com>
2020-01-29 13:40:14 -0800

    follow bezier curve
    follow by position

---
+++ Assets/BezierFollow.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierFollow : MonoBehaviour
+{
+	[SerializeField]
+	private Transform[] routes;
+	
+	private int routeToGo = 0;
+	private float tParam = 0f;
+	private Vector2 objPosition;
+	public float speedModifier = 0.5f;
+	private bool coroutineAllowed = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(coroutineAllowed)
+		{
+			StartCoroutine(GoByTheRoute(routeToGo));
+		}
+    }
+	
+	private IEnumerator GoByTheRoute(int routeNumber)
+	{
+		coroutineAllowed = false;
+		
+		Vector2 p0 = routes[routeNumber].GetChild(0).position;
+		Vector2 p1 = routes[routeNumber].GetChild(1).position;
+		Vector2 p2 = routes[routeNumber].GetChild(2).position;
+		Vector2 p3 = routes[routeNumber].GetChild(3).position;
+		
+		while(tParam < 1)
+		{
+			tParam += Time.deltaTime * speedModifier;
+			
+			objPosition = Mathf.Pow(1 - tParam, 3) * p0 + 3* Mathf.Pow(1 - tParam, 2) * tParam * p1 + 3 * (1 - tParam) * Mathf.Pow(tParam, 2) * p2 + Mathf.Pow(tParam, 3) * p3;
+			
+			transform.position = objPosition;
+			yield return new WaitForEndOfFrame();
+		}
+		
+		tParam = 0f;
+		routeToGo += 1;
+		
+		if(routeToGo > routes.Length - 1)
+		{
+			routeToGo = 0;
+		}
+		
+		coroutineAllowed = true;
+	}
+}
---
+++ Assets/Path.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour {
+	
+	private Transform[] PathNodes;
+	public float MoveSpeed;
+	public GameObject Player;
+	float Timer;
+	static Vector3 CurrentPositionHolder;
+	int CurrentNode;
+	private Vector2 startPosition;
+
+	// Use this for initialization
+	void Start ()
+	{
+		PathNodes = GetComponentsInChildren<Transform>();
+		CheckNode();
+	}
+
+	void CheckNode()
+	{
+		Timer = 0;
+		startPosition = transform.position;
+		CurrentPositionHolder = PathNodes[CurrentNode].position;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		Timer += Time.deltaTime * MoveSpeed;
+
+		if (Player.transform.position != CurrentPositionHolder)
+		{
+			Player.transform.position = Vector3.Lerp(startPosition, CurrentPositionHolder, Timer);
+		}
+		else //if(CurrentNode <PathNode.Length -1)
+		{
+			CurrentNode++;
+			//loop
+			if(CurrentNode == PathNodes.Length)
+			{
+				CurrentNode = 0;
+			}
+			CheckNode();
+		}
+	}
+}
