7d33c765d65b19f12d808fef4e8b88a6f033fcef
Benny <bennyd87708@gmail.com>
2020-01-29 22:54:06 -0800

    start trying to lock to bezier curve

--- Assets/BezierFollow.cs
+++ Assets/BezierFollow.cs
@@ -5,50 +5,22 @@ using UnityEngine;
 public class BezierFollow : MonoBehaviour
 {
 	[SerializeField]
-	private Transform[] routes;
-	
-	private int routeToGo = 0;
-	private float tParam = 0f;
-	private Vector2 objPosition;
-	public float speedModifier = 0.5f;
-	private bool coroutineAllowed = true;
+	private Transform route;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if(coroutineAllowed)
-		{
-			StartCoroutine(GoByTheRoute(routeToGo));
-		}
+        
     }
 	
-	private IEnumerator GoByTheRoute(int routeNumber)
+	private Vector3 bezierPosition(float t)
 	{
-		coroutineAllowed = false;
-		
-		Vector2 p0 = routes[routeNumber].GetChild(0).position;
-		Vector2 p1 = routes[routeNumber].GetChild(1).position;
-		Vector2 p2 = routes[routeNumber].GetChild(2).position;
-		Vector2 p3 = routes[routeNumber].GetChild(3).position;
-		
-		while(tParam < 1)
-		{
-			tParam += Time.deltaTime * speedModifier;
-			
-			objPosition = Mathf.Pow(1 - tParam, 3) * p0 + 3* Mathf.Pow(1 - tParam, 2) * tParam * p1 + 3 * (1 - tParam) * Mathf.Pow(tParam, 2) * p2 + Mathf.Pow(tParam, 3) * p3;
-			
-			transform.position = objPosition;
-			yield return new WaitForEndOfFrame();
-		}
-		
-		tParam = 0f;
-		routeToGo += 1;
-		
-		if(routeToGo > routes.Length - 1)
-		{
-			routeToGo = 0;
-		}
-		
-		coroutineAllowed = true;
+		Vector2 p0 = route.GetChild(0).position;
+		Vector2 p1 = route.GetChild(1).position;
+		Vector2 p2 = route.GetChild(2).position;
+		Vector2 p3 = route.GetChild(3).position;
+		return Mathf.Pow(1 - t, 3) * p0 + 3* Mathf.Pow(1 - t, 2) * t * p1 + 3 * (1 - t) * Mathf.Pow(t, 2) * p2 + Mathf.Pow(t, 3) * p3;
 	}
+	
+	
 }
