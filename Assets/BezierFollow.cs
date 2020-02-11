6153d241e2fc026fc811a1f43686218ad015147d
Benny <bennyd87708@gmail.com>
2020-02-11 09:59:51 -0800

    finish toggleable strict movement

--- Assets/BezierFollow.cs
+++ Assets/BezierFollow.cs
@@ -18,19 +18,32 @@ public class BezierFollow : MonoBehaviour
 		
 		//print(route.GetComponent<Route>().bezierPosition(route.GetComponent<Route>().nearestPointT(transform.position))); 
     }
-	/*
+	
 	void FixedUpdate()
+	{		
+		if(strictMovement)
+		{
+			StartCoroutine(GoByTheRoute());
+		} else {
+			updatePosition();
+		}
+		
+		if(GetComponent<TimeBody>().frame == -1)
+		{
+			GetComponent<Rigidbody>().isKinematic = false;
+		}
+	}
+	
+	void updatePosition()
 	{
 		//GetComponent<Rigidbody>().AddForce(new Vector3(0, 20, 0));
 		//print("adding force");
 		//GetComponent<Rigidbody>().velocity += new Vector3(0,-9.81f,0) * Time.deltaTime;
 		GetComponent<Rigidbody>().velocity = route.GetComponent<Route>().bezierPosition(route.GetComponent<Route>().nearestPointT(transform.position + GetComponent<Rigidbody>().velocity)) - new Vector2(transform.position.x, transform.position.y);
 		Vector3 nextPos = route.GetComponent<Route>().bezierPosition(route.GetComponent<Route>().nearestPointT(transform.position));
-		Debug.DrawLine(transform.position, nextPos, depthTest: false);
+		//Debug.DrawLine(transform.position, nextPos, Color.black, false);
 		transform.position = nextPos;
 	}
-	*/
-
 	
 	private IEnumerator GoByTheRoute()
 	{
@@ -50,18 +63,5 @@ public class BezierFollow : MonoBehaviour
 		
 		transform.position = new Vector3(-15, 0, -15);
 		GetComponent<Rigidbody>().isKinematic = true;
-	}
-	
-	void FixedUpdate()
-	{		
-		if(strictMovement)
-		{
-			StartCoroutine(GoByTheRoute());
-		}
-		
-		if(GetComponent<TimeBody>().frame == -1)
-		{
-			GetComponent<Rigidbody>().isKinematic = false;
-		}
 	}
 }
