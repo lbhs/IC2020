206d734a057b08af8835afb9f2055aa40057719a
sheldonmckinsey <59886177+sheldonmckinsey@users.noreply.github.com>
2020-02-25 09:58:43 -0800

    Colored Photons

--- Assets/photonParts.cs
+++ Assets/photonParts.cs
@@ -6,14 +6,23 @@ using UnityEngine.UI;
 public class photonParts : MonoBehaviour
 {
 	public Rigidbody photon;
+	public Rigidbody bluephoton;
+	public Rigidbody uvphoton;
 	public Vector3 velocity ;
+	
     // Start is called before the first frame update
     void Start()
     {
        // photonSpawn = GameObject.GetComponent<Rigidbody>();
 	   photon = gameObject.GetComponent<Rigidbody>();
+	   bluephoton = gameObject.GetComponent<Rigidbody>();
+	   uvphoton = gameObject.GetComponent<Rigidbody>();
 	   photon.isKinematic = false;
+	   bluephoton.isKinematic = false;
+	   
 	   photon.velocity = velocity;
+	   bluephoton.velocity = velocity;
+	   uvphoton.velocity = velocity;
 	   //Destroy(gameObject, 2);
 	   
     }
