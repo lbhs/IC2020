bf458a2bd9093c125e275f4cf86a193a27c1a85b
Jubliani <59886167+Jubliani@users.noreply.github.com>
2020-03-04 13:05:43 -0800

    invaderMovementRevamped

---
+++ Assets/invaderMovementRevamped.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class invaderMovementRevamped : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody target;
+    // public Vector3 targetVelocity;
+    public Vector3 targetPosition;
+    private int transitNumber;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = gameObject.GetComponent<Rigidbody>();
+        targetPosition = target.position;
+        //targetVelocity = target.velocity;
+        transitNumber = 0;
+
+    }
+
+    public void move()
+    {
+        if (transitNumber % 2 == 0)
+        {
+            if (Mathf.Abs(targetPosition.x - target.position.x) > 9)
+            {
+                moveDown();
+            }
+            else
+            {
+                target.velocity = new Vector3(2, 0, 0);
+            }
+        }
+        else
+        {
+            if (Mathf.Abs(targetPosition.x - target.position.x) > 9)
+            {
+                moveDown();
+            }
+            else
+            {
+                target.velocity = new Vector3(-2, 0, 0);
+            }
+        }
+    }
+
+    public void moveDown()
+    {
+        if (Mathf.Abs(targetPosition.y - target.position.y) < 1)
+        {
+            target.velocity = new Vector3(0, -1, 0);
+        }
+        else
+        {
+            target.velocity = new Vector3(0, 0, 0);
+            targetPosition = target.position;
+            transitNumber += 1;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+}
