e246bffbe472c2cb28bf2043c91141eeca720d4e
bennyd87708 <bennyd87708@gmail.com>
2020-03-09 20:31:18 -0700

    back to the drawing board speed wise

--- Assets/BezierFollow.cs
+++ Assets/BezierFollow.cs
@@ -10,14 +10,14 @@ public class BezierFollow : MonoBehaviour
 	public float speed;
 	public float t = 0f;
 	public bool strictMovement = true;
-	private List<double> realPosition;
-	private List<double> realVelocity = new List<double>(){0,0,0};
+	public Vector3 fakeVelocity;
+	private double x;
+	private double y;
 
     void Start()
     {
-		realPosition = route.GetComponent<Route>().toList(transform.position);
-		print(route.GetComponent<Route>().bezierPosition(route.GetComponent<Route>().nearestPointT(transform.position, 0.000001f))[0]);
-		print(route.GetComponent<Route>().nearestCurvePoint(realPosition)[0]);
+		print(route.GetComponent<Route>().bezierPosition(route.GetComponent<Route>().nearestPointT(transform.position, 0.000001f)).x);
+		print(route.GetComponent<Route>().nearestCurvePoint(transform.position).x);
 	}
 	
 	void FixedUpdate()
@@ -36,27 +36,10 @@ public class BezierFollow : MonoBehaviour
 	}
 	
 	void updatePosition()
-	{
-		//artificial gravity - should exactly mimic rigidbody gravity but here just in case if we want to make sure gravity is calculated BEFORE the position adjustments
-		//GetComponent<Rigidbody>().velocity += new Vector3(0,-9.81f,0) * Time.deltaTime;
-		
-		//add fake velocity change to real velocity
-		realVelocity = route.GetComponent<Route>().combine(route.GetComponent<Route>().toList(GetComponent<Rigidbody>().velocity), realVelocity);
-		
-		//calculate new position on curve based on velocity
-		List<double> nextPos = route.GetComponent<Route>().nearestCurvePoint(route.GetComponent<Route>().combine(realPosition, realVelocity));
-		
-		//keep track of change in velocity that keeping it on the curve has induced
-		realVelocity = route.GetComponent<Route>().decombine(nextPos, realPosition);
-		
-		//update accurate position for next frame
-		realPosition = nextPos;
-		
-		//update actual visual of particle with the right position
-		transform.position = route.GetComponent<Route>().toVector(realPosition);
-		
-		//keep visual velocity on 0 so it stays on the curve (velocity is still accounted for behind the scenes)
-		//GetComponent<Rigidbody>().velocity = Vector3.zero;
+	{		
+		Vector3 nextPos = route.GetComponent<Route>().nearestCurvePoint(transform.position + GetComponent<Rigidbody>().velocity);
+		GetComponent<Rigidbody>().velocity = (nextPos - transform.position);
+		transform.position = nextPos;
 	}
 	
 	private IEnumerator GoByTheRoute()
@@ -70,7 +53,7 @@ public class BezierFollow : MonoBehaviour
 			}
 			
 			t += Time.deltaTime * speed;
-			transform.position = route.GetComponent<Route>().toVector(route.GetComponent<Route>().bezierPosition(t));
+			transform.position = route.GetComponent<Route>().bezierPosition(t);
 			
 			yield return new WaitForEndOfFrame();
 		}
--- Assets/Route.cs
+++ Assets/Route.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using IC2020;
 using System.Linq;
-using System;
 
 public class Route : MonoBehaviour
 {
@@ -14,9 +13,9 @@ public class Route : MonoBehaviour
 	
 	private void OnDrawGizmos()
 	{
-		for (double t = 0; t <= 1; t += 0.025f)
+		for (float t = 0; t <= 1; t += 0.025f)
 		{
-			Gizmos.DrawSphere(toVector(bezierPosition(t)), 0.25f);
+			Gizmos.DrawSphere(bezierPosition(t), 0.25f);
 		}
 		
 		//Gizmos.DrawSphere(bezierPosition(0.5f), 0.5f);
@@ -32,12 +31,12 @@ public class Route : MonoBehaviour
 	public float nearestPointT(Vector2 pos, float accuracy)
 	{
 		float nearestT = 0f;
-		float nearestDistance = Vector2.Distance(toVector(bezierPosition(0f)), pos);
+		float nearestDistance = Vector2.Distance(bezierPosition(0f), pos);
 		float currentDistance;
 		
 		for(float t = 0f; t <= 1f; t += accuracy) //increment increases accuracy - need to make less resource intensive - use derivative?
 		{
-			currentDistance = Vector2.Distance(toVector(bezierPosition(t)), pos);
+			currentDistance = Vector2.Distance(bezierPosition(t), pos);
 			if(currentDistance < nearestDistance)
 			{
 				nearestDistance = currentDistance;
@@ -47,10 +46,10 @@ public class Route : MonoBehaviour
 		return nearestT;
 	}
 	
-	public double newtonsMethod(List<double> point, double currentPos, int iterations)
+	public float newtonsMethod(Vector2 point, float currentPos, int iterations)
 	{
-		double nextPos = currentPos - (funcDY(currentPos, point) / funcDDY(currentPos, point));
-		print(bezierPosition(nextPos)[0]);
+		float nextPos = currentPos - (funcDY(currentPos, point) / funcDDY(currentPos, point));
+		//print(bezierPosition(nextPos).x);
 		if(iterations == 1)
 		{
 			return nextPos;
@@ -58,56 +57,56 @@ public class Route : MonoBehaviour
 		return newtonsMethod(point, nextPos, iterations - 1);
 	}
 	
-	public List<double> nearestCurvePoint(List<double> point)
+	public Vector2 nearestCurvePoint(Vector2 point)
 	{
-		List<double> initial = new List<double>(){0f, 0.5f, 1f};
-		double quadraticEstimate = minFuncP(point, quadraticMethod(point, initial, 4));
-		//print(bezierPosition(quadraticEstimate)[0]);
-		//double newtonsEstimate = newtonsMethod(point, quadraticEstimate, 5);
+		List<float> initial = new List<float>(){0f, 0.5f, 1f};
+		float quadraticEstimate = minFuncP(point, quadraticMethod(point, initial, 5));
+		//print(bezierPosition(quadraticEstimate).x);
+		float newtonsEstimate = newtonsMethod(point, quadraticEstimate, 5);
 		return bezierPosition(quadraticEstimate);
 	}
 	
-	public double funcD(double t, List<double> pos)
+	public float funcD(float t, Vector2 pos)
 	{
-		List<double> point = bezierPosition(t);
-		return ((pos[0] - point[0]) * (pos[0] - point[0])) + ((pos[1] - point[1]) * (pos[1] - point[1]));
+		Vector2 point = bezierPosition(t);
+		return ((pos.x - point.x) * (pos.x - point.x)) + ((pos.y - point.y) * (pos.y - point.y));
 	}
 	
-	public double funcDY(double t, List<double> pos)
+	public float funcDY(float t, Vector2 pos)
 	{
-		List<double> point = bezierPosition(t);
-		List<double> slope = bezierSlope(t);
-		return 2 * ((point[0] - pos[0]) * slope[0]) + ((point[1] - pos[1]) * slope[1]);
+		Vector2 point = bezierPosition(t);
+		Vector2 slope = bezierSlope(t);
+		return 2 * ((point.x - pos.x) * slope.x) + ((point.y - pos.y) * slope.y);
 	}
 	
-	public double funcDDY(double t, List<double> pos)
+	public float funcDDY(float t, Vector2 pos)
 	{
-		List<double> point = bezierPosition(t);
-		List<double> slope = bezierSlope(t);
-		List<double> acceleration = bezierAcceleration(t);
-		return 2 * ((slope[0] * slope[0])+(acceleration[0] * (point[0] - pos[0]))+(slope[1] * slope[1])+(acceleration[1] * (point[1] - pos[1])));
+		Vector2 point = bezierPosition(t);
+		Vector2 slope = bezierSlope(t);
+		Vector2 acceleration = bezierAcceleration(t);
+		return 2 * ((slope.x * slope.x)+(acceleration.x * (point.x - pos.x))+(slope.y * slope.y)+(acceleration.y * (point.y - pos.y)));
 	}
 	
-	public double minFuncP(List<double> point, List<double> slist)
+	public float minFuncP(Vector2 point, List<float> slist)
 	{
-		double y23 = ((slist[1] * slist[1]) - (slist[2] * slist[2])) * funcD(slist[0], point);
-		double y31 = ((slist[2] * slist[2]) - (slist[0] * slist[0])) * funcD(slist[1], point);
-		double y12 = ((slist[0] * slist[0]) - (slist[1] * slist[1])) * funcD(slist[2], point);
-		double s23 = (slist[1] - slist[2]) * funcD(slist[0], point);
-		double s31 = (slist[2] - slist[0]) * funcD(slist[1], point);
-		double s12 = (slist[0] - slist[1]) * funcD(slist[2], point);
+		float y23 = ((slist[1] * slist[1]) - (slist[2] * slist[2])) * funcD(slist[0], point);
+		float y31 = ((slist[2] * slist[2]) - (slist[0] * slist[0])) * funcD(slist[1], point);
+		float y12 = ((slist[0] * slist[0]) - (slist[1] * slist[1])) * funcD(slist[2], point);
+		float s23 = (slist[1] - slist[2]) * funcD(slist[0], point);
+		float s31 = (slist[2] - slist[0]) * funcD(slist[1], point);
+		float s12 = (slist[0] - slist[1]) * funcD(slist[2], point);
 		
 		return 0.5f * ((y23 + y31 + y12)/(s23 + s31 + s12));
 	}
 	
-	public List<double> quadraticMethod(List<double> point, List<double> slist, int iterations)
+	public List<float> quadraticMethod(Vector2 point, List<float> slist, int iterations)
 	{
-		double star = minFuncP(point, slist);
+		float star = minFuncP(point, slist);
 		slist.Add(star);
 		//maybe rewrite to not create new list each time
-		List<double> PValues = new List<double>(){funcP(slist[0], slist, point), funcP(slist[1], slist, point), funcP(slist[2], slist, point), funcP(slist[3], slist, point)};
+		List<float> PValues = new List<float>(){funcP(slist[0], slist, point), funcP(slist[1], slist, point), funcP(slist[2], slist, point), funcP(slist[3], slist, point)};
 		/*debugging info
-		foreach(double item in PValues)
+		foreach(float item in PValues)
 		{
 			print(item);
 		}
@@ -121,48 +120,48 @@ public class Route : MonoBehaviour
 		return quadraticMethod(point, slist, iterations - 1);
 	}
 	
-	public double funcP(double t, List<double> slist, List<double> point)
+	public float funcP(float t, List<float> slist, Vector2 point)
 	{
-		double first = ((t - slist[1])*(t - slist[2])*funcD(slist[0], point))/((slist[0] - slist[1])*(slist[0] - slist[2]));
-		double second = ((t - slist[0])*(t - slist[2])*funcD(slist[1], point))/((slist[1] - slist[0])*(slist[1] - slist[2]));
-		double third = ((t - slist[0])*(t - slist[1])*funcD(slist[2], point))/((slist[2] - slist[0])*(slist[2] - slist[1]));
+		float first = ((t - slist[1])*(t - slist[2])*funcD(slist[0], point))/((slist[0] - slist[1])*(slist[0] - slist[2]));
+		float second = ((t - slist[0])*(t - slist[2])*funcD(slist[1], point))/((slist[1] - slist[0])*(slist[1] - slist[2]));
+		float third = ((t - slist[0])*(t - slist[1])*funcD(slist[2], point))/((slist[2] - slist[0])*(slist[2] - slist[1]));
 		return funcD((first + second + third), point);
 	}
 	
-	public double funcB(int n, int i, double u)
+	public float funcB(int n, int i, float u)
 	{
-		return (factorial(n)/(factorial(i)*factorial(n - i))) * Math.Pow(1 - u, n - i) * Math.Pow(u, i);
+		return (factorial(n)/(factorial(i)*factorial(n - i))) * Mathf.Pow(1 - u, n - i) * Mathf.Pow(u, i);
 	}
 	
-	public List<double> bezierPosition(double t)
+	public Vector2 bezierPosition(float t)
 	{
-		List<double> summation = new List<double>(){0,0};
+		Vector2 summation = Vector2.zero;
 		int n = controlPoints.Length - 1;
 		for(int i = 0; i <= n; i++)
 		{
-			summation = combine(summation, distribute((new List<double>(){controlPoints[i].position.x,controlPoints[i].position.y}), funcB(n, i, t)));
+			summation += funcB(n, i, t) * (Vector2)controlPoints[i].position;
 		}
 		return summation;
 	}
 	
-	public List<double> bezierSlope(double t)
+	public Vector2 bezierSlope(float t)
 	{
-		List<double> summation = new List<double>(){0,0};
+		Vector2 summation = Vector2.zero;
 		int n = controlPoints.Length - 1;
 		for(int i = 0; i <= (n - 1); i++)
 		{
-			summation = combine(summation, distribute((new List<double>(){controlPoints[i+1].position.x-controlPoints[i].position.x,controlPoints[i+1].position.y-controlPoints[i].position.y}), (funcB(n-1, i, t) * n)));
+			summation += funcB(n-1, i, t) * n * (Vector2)(controlPoints[i+1].position - controlPoints[i].position);
 		}
 		return summation;
 	}	
 	
-	public List<double> bezierAcceleration(double t)
+	public Vector2 bezierAcceleration(float t)
 	{
-		List<double> summation = new List<double>(){0,0};
+		Vector2 summation = Vector2.zero;
 		int n = controlPoints.Length - 1;
 		for(int i = 0; i <= (n - 2); i++)
 		{
-			summation = combine(summation, distribute(toList(controlPoints[i+2].position - (2 * controlPoints[i+1].position) - controlPoints[i].position), (funcB(n-2, i, t) * n * (n-1))));
+			summation += funcB(n-2, i, t) * n * (n-1) * (Vector2)(controlPoints[i+2].position - (2 * controlPoints[i+1].position) - controlPoints[i].position);
 		}
 		return summation;
 	}
@@ -175,46 +174,5 @@ public class Route : MonoBehaviour
 			val *= i;
 		}
 		return val;
-	}
-	
-	public Vector3 toVector(List<double> pos)
-	{
-		if(pos.Count == 2)
-		{
-			pos.Add(0);
-		}
-		return new Vector3((float)pos[0], (float)pos[1], (float)pos[2]);
-	}
-	
-	public List<double> toList(Vector3 pos)
-	{
-		return new List<double>(){(double)pos.x, (double)pos.y, (double)pos.z};
-	}
-	
-	public List<double> distribute(List<double> doubles, double magnitude)
-	{
-		for(int i = 0; i < doubles.Count; i++)
-		{
-			doubles[i] *= magnitude;
-		}
-		return doubles;
-	}
-	
-	public List<double> combine(List<double> first, List<double> second)
-	{
-		for(int i = 0; i < second.Count; i++)
-		{
-			first[i] += second[i];
-		}
-		return first;
-	}
-	
-	public List<double> decombine(List<double> first, List<double> second)
-	{
-		for(int i = 0; i < first.Count; i++)
-		{
-			first[i] -= second[i];
-		}
-		return first;
 	}
 }
