7c011fac460f4b24a6c47d9654aeedd7413733e6
sheldonmckinsey <59886177+sheldonmckinsey@users.noreply.github.com>
2020-03-12 11:05:43 -0700

    Other RXNs

---
+++ Assets/HClChlorineRXN.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HClChlorineRXN : MonoBehaviour
+{
+	public GameObject HCl;
+	public GameObject Hydrogen;
+	public GameObject HTwo;
+	private Vector3 ColPosition;
+	private Rigidbody ClAtom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClAtom = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	void OnCollisionEnter(Collision collider)
+	{
+		if(collider.gameObject.tag == "HMol")
+		{
+			ColPosition = collider.gameObject.transform.position;
+			
+			Destroy(gameObject);
+			Destroy(collider.gameObject);
+			Instantiate(HCl, new Vector3(ColPosition.x + 1, ColPosition.y, 0), Quaternion.identity);
+			Instantiate(Hydrogen, new Vector3(ColPosition.x, ColPosition.y, 0), Quaternion.identity);
+			Instantiate(HTwo, new Vector3(-1f, 4f, 0), Quaternion.identity);
+		}
+	}
+}
--- Assets/HClHydrogenRXN.cs
+++ Assets/HClHydrogenRXN.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class HClHydrogenRXN : MonoBehaviour
 {
 	public GameObject HCl;
+	public GameObject Chlorine;
+	public GameObject ClTwo;
 	private Vector3 ColPosition;
-	private HClChlorineMolecule HClChlorineMolecule;
+	//private HClChlorineMolecule HClChlorineMolecule;
 	private Rigidbody HAtom;
     // Start is called before the first frame update
     void Start()
@@ -30,6 +32,16 @@ public class HClHydrogenRXN : MonoBehaviour
 			Destroy(collider.gameObject);
 			//Instantiate(HCl, ColPosition, Quaternion.identity);
 			Instantiate(HCl, new Vector3(ColPosition.x + 1, ColPosition.y, 0), Quaternion.identity);
+		}
+		if(collider.gameObject.tag == "ChlorineMol")
+		{
+			ColPosition = collider.gameObject.transform.position;
+			
+			Destroy(gameObject);
+			Destroy(collider.gameObject);
+			Instantiate(HCl, new Vector3(ColPosition.x + 1, ColPosition.y, 0), Quaternion.identity);
+			Instantiate(Chlorine, new Vector3(ColPosition.x - 1, ColPosition.y, 0), Quaternion.identity);
+			Instantiate(ClTwo, new Vector3(-1f, 5.2f, 0), Quaternion.identity);
 		}
 	}
 }
