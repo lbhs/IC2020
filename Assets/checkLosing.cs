4dad64285f257676f30daa2ddba75050d1522cdf
Jubliani <59886167+Jubliani@users.noreply.github.com>
2020-03-12 11:35:47 -0700

    Game Over screen

---
+++ Assets/checkLosing.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class checkLosing : MonoBehaviour
+{
+    public Text loseText;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collider)
+    {
+        print(collider.gameObject.tag);
+
+        if (collider.gameObject.tag == "ChlorineMol" || collider.gameObject.tag == "HMol")
+        {
+            collider.gameObject.transform.position = new Vector3(-15, 0, -15);
+            print("lose");
+            GameObject.Find("LoseText").GetComponent<RectTransform>().localScale = Vector3.one;
+            Time.timeScale = 0;
+        }
+    }
+}
