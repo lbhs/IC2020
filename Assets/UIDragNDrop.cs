8d88a14172d9c9129fbe90597dd9e99c522a0192
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-03-23 15:17:34 -0700

    dragNdrop scripts

---
+++ Assets/UIDragNDrop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIDragNDrop : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    public bool returnToZero = false; //default value is false
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (returnToZero == true)
+        {
+            transform.localPosition = Vector3.zero;
+        }
+    }
+}
