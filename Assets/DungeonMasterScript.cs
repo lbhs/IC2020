ab3b7565c7b04d074e44fd5c929318b4c4cc56fd
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-03-24 11:20:08 -0700

    added graphics for UI and end turn buttion

---
+++ Assets/DungeonMasterScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+#pragma warning disable CS0618
+
+public class DungeonMasterScript : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
