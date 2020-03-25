5bd3e6e40fd203011a79be951a50ecaaf2e6df12
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-03-24 17:45:46 -0700

    fixed bugs

--- Assets/AtomController.cs
+++ Assets/AtomController.cs
@@ -10,7 +10,7 @@ public class AtomController : MonoBehaviour
     private GameObject TurnScreen;
 
     public List<GameObject> variants = new List<GameObject>();
-    private int variantsCounter = 0;
+    private int variantCounter;
 
 
     void Update()
@@ -24,43 +24,27 @@ public class AtomController : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.LeftShift))
             {
-                //transform.Rotate(new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z + 45));
                 transform.rotation = transform.rotation * Quaternion.Euler(0, 0, -45);
             }
             else if (Input.GetKey(KeyCode.LeftControl))
             {
-                if (variantsCounter == 0)
+                variants[variantCounter].SetActive(false);
+                variantCounter++;
+                if(variantCounter >= variants.Count)
                 {
-                    if (variantsCounter < variants.Count)
-                    {
-                        variantsCounter++;
-                        variants[0].SetActive(false);
-                        variants[1].SetActive(true);
-                    }
-                    else { return; }
+                    variantCounter = 0;
                 }
-                else if (variantsCounter == 1)
-                {
-                    if (variantsCounter < variants.Count)
-                    {
-                        variantsCounter++;
-                        variants[1].SetActive(false);
-                        variants[2].SetActive(true);
-                    }
-                    else { return; }
-                }
-
+                variants[variantCounter].SetActive(true);
+            }
+            else
+            {
+                mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+                // Store offset = gameobject world pos - mouse world pos
+                mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
             }
         }
-        else
-        {
-            mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-            // Store offset = gameobject world pos - mouse world pos
-            mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
-        }
+
     }
-
-
 
     void OnMouseDrag()
     {
@@ -70,10 +54,13 @@ public class AtomController : MonoBehaviour
             {
                 return;
             }
+            else if (Input.GetKey(KeyCode.LeftControl))
+            {
+                return;
+            }
             else
             {
                 transform.position = GetMouseAsWorldPoint() + mOffset;
-                transform.position = new Vector3(transform.position.z, transform.position.y, 0);
             }
         }
     }
@@ -83,7 +70,7 @@ public class AtomController : MonoBehaviour
         // Pixel coordinates of mouse (x,y)
         Vector3 mousePoint = Input.mousePosition;
         // z coordinate of game object on screen
-        mousePoint.z = 0;
+        mousePoint.z = mZCoord;
         // Convert it to world points
         return Camera.main.ScreenToWorldPoint(mousePoint);
     }
