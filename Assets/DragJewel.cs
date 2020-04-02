959c5e88b32eca5e0aa238f2357adf84751ff1d3
Jubliani <59886167+Jubliani@users.noreply.github.com>
2020-04-01 23:04:44 -0700

    Attempted dragging jewel script

---
+++ Assets/DragJewel.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragJewel : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    private RectTransform rectTransform;
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition = eventData.delta;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("yes");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
