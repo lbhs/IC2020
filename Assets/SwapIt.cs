ab7ddeb7fed1a2a03bd7eb5024a7b4e89036f3d1
bennyd87708 <bennyd87708@gmail.com>
2020-04-20 14:07:25 -0700

    swap it

---
+++ Assets/SwapIt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapIt : MonoBehaviour
+{
+	[SerializeField]
+	private Object PrefabToBecome; 
+	
+	void OnMouseOver()
+	{
+		Debug.Log(GetComponent<FixedJoint2D>());
+		if(Input.GetMouseButtonDown(2) && GetComponent<BondMaker>().bonded == false)
+		{
+			Instantiate(PrefabToBecome, transform.position, Quaternion.identity);
+			Destroy(gameObject);
+		}
+    }
+}
