dfc8a280257435db96cb5e300f1d5c88507b97b8
bennyd87708 <bennyd87708@gmail.com>
2020-04-21 20:20:38 -0700

    Merge April 21 Sogo into CROWN-JOULES-SOLITAIRE
    * Tuesday April 21 Sogo Upgrade
    
    Co-authored-by: sgsogo <43896422+sgsogo@users.noreply.github.com>

---
+++ Assets/CannotBreakBond.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CannotBreakBond : MonoBehaviour
+{
+    public Text NotEnoughJoulesTextbox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NotEnoughJoulesTextbox.text = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Denied()
+    {
+        NotEnoughJoulesTextbox.text = "You don't have enough Joules to break this bond!";
+        StartCoroutine(countdown());
+    }
+
+    private IEnumerator countdown()  //this is a co-routine, can run in parallel with other scripts/functions
+    {
+        yield return new WaitForSeconds(5);
+        NotEnoughJoulesTextbox.text = null;
+        yield break;
+    }
+}
+
+
+
