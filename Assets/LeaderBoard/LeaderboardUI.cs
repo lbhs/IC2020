8049d5cdcf364c61f730b93bb3d86b9c654eb21e
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-04-27 16:53:19 -0700

    initial leaderboard

---
+++ Assets/LeaderBoard/LeaderboardUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderboardUI : MonoBehaviour
+{
+    public GameObject InputFeildUI;
+    public InputField IFEmail;
+    public GameOver GameOverScript;
+    public LeaderboardScript LBS;
+    public GameObject errorText;
+    public GameObject successText;
+
+    public void LBSendButtonFunction()
+    {
+        if (IFEmail.text != null)
+        {
+            string n = IFEmail.text;
+            int s = GameOverScript.FinalGameOverScore;
+            bool b = LBS.Send(n, s);
+            if (b == false)
+            {
+                errorText.SetActive(true);
+            }
+            else
+            {
+                successText.SetActive(true);
+                InputFeildUI.SetActive(false);
+            }
+        }
+        else
+        {
+            errorText.SetActive(true);
+        }
+    }
+}
