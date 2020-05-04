b69ca40ca4560ff0b1622a0b83de71a20777c6a8
KennethChu2441 <52292475+KennethChu2441@users.noreply.github.com>
2020-05-04 13:58:47 -0700

    Added medals
    Medals pop up on gamover scenel. Working on fade in script, but not working in this version.

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -8,6 +8,11 @@ public class GameOver : MonoBehaviour
   public int FinalGameOverScore;
   public Text FinalScoreText;
   public int CurrentDisplayScore = 0;
+  public GameObject CopperMedal;
+  public GameObject SilverMedal;
+  public GameObject GoldMedal;
+  public GameObject PlatMedal;
+  public bool PointCountUpConcluded = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -60,7 +65,42 @@ public class GameOver : MonoBehaviour
         FinalScoreText.text = " ";
         yield return new WaitForSeconds (0.5f);
 
+
       }
+    }
+
+    IEnumerator InstantiateCopper()            //Can't get the fade in to work, but since it doesn't do anything I'm leaving the code here. Would be fantastic if someone figured out how to make it work!
+    {
+      for (float i = 0f; i <=1f ; i += .01f)
+      {
+        SpriteRenderer CopperRenderer = CopperMedal.GetComponent<SpriteRenderer>();
+        Color CopperColor = CopperRenderer.color;
+        CopperColor.a = i;
+        CopperRenderer.color = CopperColor;
+        Debug.Log(CopperColor.a);
+        yield return new WaitForSeconds (0.025f);
+
+      }
+      Debug.Log("Copper Medal faded in");
+      SpriteRenderer testCopperMedal = CopperMedal.GetComponent<SpriteRenderer>();
+      var TestColor = testCopperMedal.color;
+      Debug.Log(TestColor.a);
+
+    }
+    IEnumerator InstantiateSilver()
+    {
+      yield return new WaitForSeconds (0.75f);
+      Instantiate(SilverMedal, new Vector3( 4.58f, 1.09f, 0f), Quaternion.identity);
+    }
+    IEnumerator InstantiateGold()
+    {
+      yield return new WaitForSeconds (0.75f);
+      Instantiate(GoldMedal, new Vector3(3.49f, 1.1f, 0f), Quaternion.identity);
+    }
+    IEnumerator InstantiatePlat()
+    {
+      yield return new WaitForSeconds(0.75f);
+      Instantiate(PlatMedal, new Vector3(5.14f, 0.47f, 0f), Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -68,7 +108,40 @@ public class GameOver : MonoBehaviour
     {
       if(CurrentDisplayScore == FinalGameOverScore)
       {
-        StartCoroutine(BlinkScore());
+        if (PointCountUpConcluded == false)
+        {
+
+
+          StartCoroutine(BlinkScore());
+          if (FinalGameOverScore < 300)
+            {
+              Instantiate (CopperMedal, new Vector3( 3.69f, 1.13f, 0f), Quaternion.identity);
+              Debug.Log("Copper Medal Instantiated");
+              SpriteRenderer testCopperMedal = CopperMedal.GetComponent<SpriteRenderer>();
+              var TestColor = testCopperMedal.color;
+              Debug.Log(TestColor.a);
+              StartCoroutine(InstantiateCopper());
+              Debug.Log("Coroutine initiated and ended?");
+
+              PointCountUpConcluded = true;
+            }
+          else if (FinalGameOverScore >= 300 && FinalGameOverScore < 350)
+            {
+                StartCoroutine(InstantiateSilver());
+                PointCountUpConcluded = true;
+            }
+          else if (FinalGameOverScore >= 350 && FinalGameOverScore < 380)
+            {
+              StartCoroutine(InstantiateGold());
+              PointCountUpConcluded = true;
+            }
+          else if (FinalGameOverScore >= 380)
+            {
+              StartCoroutine(InstantiatePlat());
+              PointCountUpConcluded = true;
+            }
+
+        }
       }
 
     }
