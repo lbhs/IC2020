08b2aca336f8fcdc4debe844914012edae770682
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-05-18 14:40:10 -0700

    Single-name leaderbaord

--- Assets/LeaderBoard/UpdateLeaderboard.cs
+++ Assets/LeaderBoard/UpdateLeaderboard.cs
@@ -11,35 +11,26 @@ public class Player
     public int Score;
 }
 
-public class Team
-{
-    public string TeamName;
-    public string P1, P2, P3;
-    public List<int> Score = new List<int>();
-}
-
 public class UpdateLeaderboard : MonoBehaviour
 {
     public List<Player> players = new List<Player>();
-    public List<Team> teams = new List<Team>();
     public GameObject[] textBoxes;
 
     private void OnEnable()
     {
         clear();
         StartCoroutine(GetString());
-        // Google Sheet ID: "163XTG4mOdzoAZv-HHcKGoRtDum-XvTOsajDYoiO97VA" Sub Sheet name: "Leaderboard")
+        //"163XTG4mOdzoAZv-HHcKGoRtDum-XvTOsajDYoiO97VA", "Leaderboard")
     }
 
     IEnumerator GetString()
     {
-        string url = "https://sheets.googleapis.com/v4/spreadsheets/163XTG4mOdzoAZv-HHcKGoRtDum-XvTOsajDYoiO97VA/values/Leaderboard!" + "B2" + ":" + "G1001" + "?key=" + SecretKey.GSkey;
+        string url = "https://sheets.googleapis.com/v4/spreadsheets/163XTG4mOdzoAZv-HHcKGoRtDum-XvTOsajDYoiO97VA/values/Leaderboard!" + "B2" + ":" + "C1001" + "?key=" + SecretKey.GSkey;
         WWW www = new WWW(url);
         yield return www;
         string RecivedJSON;
         RecivedJSON = www.text;
         var J = JSON.Parse(RecivedJSON);
-        //Debug.Log(J);
         for (int i = 2; i < 1001; i++)
         {
             Player p = new Player();
@@ -54,56 +45,46 @@ public class UpdateLeaderboard : MonoBehaviour
             }
         }
         players = players.OrderByDescending(i => i.Score).ToList();
-
-        for (int i = 0; i < 24; i++)
-        {
-            Team t = new Team();
-            string TN = J["values"][i][2].Value;
-            string P1 = J["values"][i][3].Value;
-            string P2 = J["values"][i][4].Value;
-            string P3 = J["values"][i][5].Value;
-            t.TeamName = TN;
-            t.P1 = P1;
-            t.P2 = P2;
-            t.P3 = P3;
-            t.Score.Add(0);
-            teams.Add(t);
-            //Debug.Log(t.TeamName + " " + t.P1 + " " + t.P2 + " " + t.P3);
-        }
-
-        int forI = 0;
-        foreach (var t in teams)
-        {
-            foreach (var p in players)
-            {
-                if (p.Name == t.P1 || p.Name == t.P2 || p.Name == t.P3)
-                {
-                    //Debug.Log(p.Name + " " + t.TeamName + " " + t.P1);
-                    teams[forI].Score.Add(p.Score);
-                    //Debug.Log(teams[forI].TeamName);
-                }
-            }
-            teams[forI].Score = teams[forI].Score.OrderByDescending(i => i).ToList();
-            //Debug.Log(t.Score[0]);
-            forI++;
-        }
-
-        teams = teams.OrderByDescending(i => i.Score[0]).ToList();
         applyUpdates();
+
+        /*string url1 = "https://sheets.googleapis.com/v4/spreadsheets/163XTG4mOdzoAZv-HHcKGoRtDum-XvTOsajDYoiO97VA/values/Leaderboard!B" + row + ":B" + row + "?key=" + SecretKey.GSkey;
+        WWW www = new WWW(url1);
+        yield return www;
+        RecivedJSON = www.text;
+        var J = JSON.Parse(RecivedJSON);
+        string name = J["values"][0][0].Value;
+
+        string url2 = "https://sheets.googleapis.com/v4/spreadsheets/163XTG4mOdzoAZv-HHcKGoRtDum-XvTOsajDYoiO97VA/values/Leaderboard!C" + row + ":C" + row + "?key=" + SecretKey.GSkey;
+        WWW www2 = new WWW(url2);
+        yield return www2;
+        RecivedJSON = www2.text;
+        var J2 = JSON.Parse(RecivedJSON);
+        string score = J2["values"][0][0].Value;
+
+        if (name == null || score == null)
+        {
+            var p = new Player();
+            p.Name = name;
+            p.Score = int.Parse(score);
+            players.Add(p);
+            Debug.Log(p.Name + " " + p.Score);
+        }
+        else
+        {
+            print("found empty cell");
+        }*/
     }
 
     void applyUpdates()
     {
-
-       
-        //solitaire version code:
-
+        //players.Sort((s1, s2) => s1.Score.CompareTo(s2.Score));
+        players = players.OrderByDescending(i => i.Score).ToList();
         int num = 0;
         foreach (var item in textBoxes)
         {
             item.transform.GetChild(0).GetComponent<Text>().text = num + 1 + ":";
-            item.transform.GetChild(1).GetComponent<Text>().text = teams[num].TeamName;
-            item.transform.GetChild(2).GetComponent<Text>().text = teams[num].Score[0].ToString();
+            item.transform.GetChild(1).GetComponent<Text>().text = players[num].Name;
+            item.transform.GetChild(2).GetComponent<Text>().text = players[num].Score.ToString();
             num++;
         }
         //Debug.Log(players[players.Count-1].Score);
@@ -112,7 +93,6 @@ public class UpdateLeaderboard : MonoBehaviour
     void clear()
     {
         players.Clear();
-        teams.Clear();
         int i = 1;
         foreach (var item in textBoxes)
         {
