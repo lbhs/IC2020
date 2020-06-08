a7d494484ef7fc247a57b80e89dc49d771002d01
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-06-08 16:06:32 -0700

    Added mobile rotating

--- Assets/RotateThis.cs
+++ Assets/RotateThis.cs
@@ -22,6 +22,26 @@ public class RotateThis : MonoBehaviour
         
     }
 
-    
-    
+#if UNITY_IPHONE || UNITY_Android
+    private float TimeInital;
+    private float minTime = 0.5f;
+    private float maxTime = 1f;
+    private Vector3 InitalPos;
+    private float DistanceThreshold = 0.2f;
+    private void OnMouseDown()
+    {
+        TimeInital = Time.time;
+        InitalPos = transform.position;
+    }
+    private void OnMouseUp()
+    {
+        float ClickTime = Time.time - TimeInital;
+        float distance = Vector3.Distance(InitalPos, transform.position);
+        if (ClickTime > minTime && ClickTime < maxTime && distance < DistanceThreshold &&gameObject.GetComponent<BondMaker>().bonded == false)
+        {
+            transform.Rotate(0, 0, 90);
+        }
+    }
+#endif
+
 }
