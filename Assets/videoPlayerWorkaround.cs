3943c16ebd160d3507f0ea01f0f4fee15be489f4
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-07-30 10:34:54 -0700

    video works

---
+++ Assets/videoPlayerWorkaround.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class videoPlayerWorkaround : MonoBehaviour
+{
+    public VideoPlayer VP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        VP.url = System.IO.Path.Combine(Application.streamingAssetsPath, "4thtry.mp4");
+        VP.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
