f461c38d4c22bf000319136e39093b296b89bcee
BeanMachineStudios <Benjaminkeller@gmail.com>
2020-07-30 17:39:15 -0700

    It works now

--- Assets/videoPlayerWorkaround.cs
+++ Assets/videoPlayerWorkaround.cs
@@ -8,7 +8,11 @@ public class videoPlayerWorkaround : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+#if UNITY_WEBGL
+        VP.url = "https://lbhs.github.io/Games/Images/4thtry.mp4";
+#else
         VP.url = System.IO.Path.Combine(Application.streamingAssetsPath, "4thtry.mp4");
+#endif
         VP.Play();
     }
 }
